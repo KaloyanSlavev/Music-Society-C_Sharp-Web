5ee8f119ddaeaa4c2dc40f6bf8027a6c88b5b06b
KaloyanSlavev <45690271+KaloyanSlavev@users.noreply.github.com>
2019-05-25 22:29:45 +0300

    Add files via upload

---
+++ Classes/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Music_Society_Forum.Classes
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLength = 500)
+        {
+            if (text == null || text.Length <= maxLength)
+                return text;
+            return text.Substring(0, maxLength) + "...";
+        }
+
+        public static string CutTextHomePage(string text, int maxLength = 1500)
+        {
+            if (text == null || text.Length <= maxLength)
+                return text;
+            return text.Substring(0, maxLength) + "...";
+        }
+    }
+}
